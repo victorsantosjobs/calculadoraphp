c7465e6791ba9874d7d82446815575ad9b831807
victorsantosjobs <115560232+victorsantosjobs@users.noreply.github.com>
2023-07-14 12:31:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <form action="functions.php" method="get">
+        <input type="text" name="num01" placeholder="Numero 1">
+   
+<select name="oper">
+<label>Escolha soma ou subtração</label>
+<option value="add">Add</option>
+<option value="sub">Sub</option>
+</select>
+<input type="text" name="num02" placeholder="Numero 2">
+<button type="submit">Calcular</button>
+</form> 
+</body>
+</html>
